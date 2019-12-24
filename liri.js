9b592ff3b8875c27e2a9c2e7dde49b48b8139699
ANRAx <ramdassaj@gmail.com>
2019-12-24 08:39:52 -0500

    add log.txt file, write functions to write to log.txt, to get artist name, to run Spotify search

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 // DEPENDENCIES 
-
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Read and set enironment variables
 require("dotenv").config();
 
@@ -15,3 +15,50 @@ let fs = require("fs");
 // Initialize the Spotify API client using our client id and secret key
 let spotify = new Spotify(keys.spotify);
 
+// FUNCTIONS
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Write to the log.txt file
+let writeToLog = function(data) {
+    // Append the JSON data and add a newline to end of log.txt file
+    fs.appendFile("log.txt", JSON.stringify(data) + "\n", function (err) {
+        if (err) {
+           return console.log(err);
+        }
+        console.log("log.txt was updated!");
+    });
+};
+
+// Helper func that gets the artist name
+let getArtistNames = function (artist) {
+    return artist.name;
+};
+
+// Func to run Spotify search
+let getMeSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "What's my age again";
+    }
+
+    spotify.search({ type: "track", query: songName}, function (err, data) {
+        if (err) {
+            console.log("Error occured: " + err);
+            return; 
+        }
+
+        let songs = data.tracks.items;
+        let data = [];
+
+        for (let i = 0; i < songs.length; i++) {
+          data.push({
+              "artist(s)": songs[i].artists.map(getArtistNames),
+              "song name: ": songs[i].name,
+              "preview song: ": songs[i].preview_url,
+              "album: ": songs[i].album.name
+          });
+        }
+
+        console.log(data);
+        writeToLog(data);
+    });
+};
