dc976704d864be1b8ea33b5c76047ee11c7fe5ac
ANRAx <ramdassaj@gmail.com>
2019-12-24 08:53:17 -0500

    finish writing function for concert search

--- liri.js
+++ liri.js
@@ -62,3 +62,43 @@ let getMeSpotify = function (songName) {
         writeToLog(data);
     });
 };
+
+// Func for concert search 
+let getMyBands = function(artist) {
+    let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(queryURL).then(
+        function(response) {
+            let jsonData = response.data;
+
+            if (!jsonData.length) {
+                console.log("No results found for " + artist);
+                return;
+            }
+
+            let logData = [];
+
+            logData.push("Upcoming concerts for " + artist + ":");
+
+            for (let i = 0; i < jsonData.length; i++) {
+                let show = jsonData[i];
+                // push eachline of concert data to logData
+                // if concert doesn't have a region, display the country instead
+                // use moment to format the date
+                logData.push(
+                    show.venue.city + 
+                    "," +
+                    (show.venue.region || show.venue.country) +
+                    " at " + 
+                    show.venue.name + 
+                    " " +
+                    moment(show.datetime).format("MM/DD/YYYY")
+                );
+            }
+
+            // Print and write the concert data as a string joined by a newline
+            console.log(logData.join("\n"));
+            writeToLog(logData.join("\n"));
+        }
+    );
+};
