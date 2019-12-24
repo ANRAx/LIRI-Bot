d25fe648db6426779ce5d72cecf8f26e40ebff3e
ANRAx <ramdassaj@gmail.com>
2019-12-24 08:21:42 -0500

    create global variables in liri.js

--- liri.js
+++ liri.js
@@ -1,6 +1,17 @@
+// DEPENDENCIES 
+
+// Read and set enironment variables
 require("dotenv").config();
 
-let keys = require("./keys.js");
+// Import API Keys
+let keys = require("./keys");
 
+// Import the node-spotify-api(NPM), axiosi(NPM), momenti(NPM), and FS packages
+let Spotify = require("node-spotify-api");
+let axios = require("axios");
+let moment = require("moment");
+let fs = require("fs");
+
+// Initialize the Spotify API client using our client id and secret key
 let spotify = new Spotify(keys.spotify);
 
